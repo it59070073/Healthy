3ddf2d109a47ac9b6eb250d2a4d31d893a64e48f
pinpipo <59070073@it.kmitl.ac.th>
2018-09-19 17:27:40 +0700

    ต่อ database แบบเกือบเสร็จ

--- app/src/main/java/com/example/pinpipo/healthy/WeightFormFragment.java
+++ app/src/main/java/com/example/pinpipo/healthy/WeightFormFragment.java
@@ -39,47 +39,64 @@ public class WeightFormFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         onClickBtn();
+        back();
     }
 
     public void onClickBtn() {
         Button submit = getView().findViewById(R.id.WeightFormSave);
-        final EditText weightFormDate = getView().findViewById(R.id.WeightFormDate);
-        EditText weightFormWeight = getView().findViewById(R.id.WeightFormWeight);
-
-        final String weightFormDateStr = weightFormDate.getText().toString();
-        final String weightFormWeightStr = weightFormWeight.getText().toString();
-
-        final String uid = firebaseAuth.getCurrentUser().getUid();
-
-        final Weight weight = new Weight(weightFormDateStr, weightFormWeightStr);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (weightFormDateStr.isEmpty() || weightFormWeightStr.isEmpty()) {
-                    Log.d("WeightForm","กรุณากรอกให้ครบ");
-                    Toast.makeText(getActivity(), "กรุณากรอกให้ครบ", Toast.LENGTH_SHORT).show();
-                } else {
-                      firebaseFirestore
-                              .collection("myfitness")
-                              .document(uid)
-                              .collection("weight")
-                              .document(weightFormDateStr)
-                              .set(weight)
-                              .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                  @Override
-                                  public void onSuccess(Void aVoid) {
-                                      Log.d("WeightForm","ข้อมูลเข้าเรียบร้อย");
-                                      Toast.makeText(getActivity(), "Done", Toast.LENGTH_SHORT).show();
-                                  }
-                              }).addOnFailureListener(new OnFailureListener() {
-                                  @Override
-                                  public void onFailure(@NonNull Exception e) {
-                                      Log.d("WeightForm", "fail");
-                                      Toast.makeText(getActivity(), "Fail", Toast.LENGTH_SHORT).show();
-                                  }
-                      });
+                EditText weightFormDate = getView().findViewById(R.id.WeightFormDate);
+                EditText weightFormWeight = getView().findViewById(R.id.WeightFormWeight);
+
+                String weightFormDateStr = weightFormDate.getText().toString();
+                String weightFormWeightStr = weightFormWeight.getText().toString();
+
+                String uid = firebaseAuth.getCurrentUser().getUid();
+
+                Weight weight = new Weight(weightFormDateStr, weightFormWeightStr);
+
+                if (weightFormDateStr.isEmpty() || weightFormWeightStr.isEmpty()){
+                    Log.d("WeightForm","fail");
+                    Toast.makeText(getActivity(),"กรุณากรอกข้อมูลให้ครบ", Toast.LENGTH_SHORT).show();
+                }else{
+                    firebaseFirestore
+                            .collection("myfitness")
+                            .document(uid)
+                            .collection("weight")
+                            .document(weightFormDateStr)
+                            .set(weight)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("WeightForm","done");
+                                    Toast.makeText(getActivity(),"done", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("WeightForm","fail");
+                            Toast.makeText(getActivity(),"fail", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
+            }
+        });
+    }
+
+    public void back(){
+        Button back = getView().findViewById(R.id.WeightFormBack);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new MenuFragment())
+                        .addToBackStack(null)
+                        .commit();
             }
         });
     }
--- app/src/main/java/com/example/pinpipo/healthy/WeightFragment.java
+++ app/src/main/java/com/example/pinpipo/healthy/WeightFragment.java
@@ -11,27 +11,34 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class WeightFragment extends Fragment {
 
-    ArrayList<Weight> weight = new ArrayList<Weight>();
+    ArrayList<Weight> weight;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     public WeightFragment(){ //เวลาย้อนกลับมันจะไม่เบิล
-
+        this.weight = new ArrayList<>();
+        this.firebaseAuth = FirebaseAuth.getInstance();
+        this.firebaseFirestore = FirebaseFirestore.getInstance();
     }
 
     @Nullable
     @Override
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -40,15 +47,59 @@ public class WeightFragment extends Fragment {
 
     public void onActivityCreated (@Nullable Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        FirebaseUser mUser = mAuth.getCurrentUser();
-        final ListView weightList = (ListView) getView().findViewById(R.id.weight_list);
+        show();
+        addWeight();
+    }
+
+    public void addWeight(){
+        Button back = getView().findViewById(R.id.WeightAdd);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view, new WeightFormFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    public void show(){
+        String uid = firebaseAuth.getCurrentUser().getUid();
+        final ListView listView = getView().findViewById(R.id.weight_list);
+
         final WeightAdapter weightAdapter = new WeightAdapter(
                 getActivity(),
                 R.layout.fragment_weight_item,
                 weight
         );
-        weightList.setAdapter(weightAdapter);
+
+        listView.setAdapter(weightAdapter);
+        weightAdapter.clear();
+
+        firebaseFirestore
+                .collection("myfitness")
+                .document(uid)
+                .collection("weight")
+                .orderBy("date", Query.Direction.DESCENDING)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for(QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                            weight.add(doc.toObject(Weight.class));
+                        }
+                        weightAdapter.notifyDataSetChanged();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("HISTORY", "kuyraiymoe");
+                        Toast.makeText(getActivity(), "Some error was found !!", Toast.LENGTH_SHORT).show();
+                    }
+        });
     }
 
 
